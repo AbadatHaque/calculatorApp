ae6684579c4a08cff33b709203920b9fd08d891c
coreventum-frontend <frontend@coreventum.com>
2025-02-25 17:49:18 +0530

    working on pwa

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,22 @@
-const CACHE_NAME = "v1";
-const urlsToCache = ["/", "/index.html", "/CSS/style.css"];
-
-self.addEventListener("install", (event) => {   
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open('calculator-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/CSS/style.css',
+          '/images/Calculator_512.png',
+          // Add any other assets that should be cached
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      caches.match(e.request).then((cachedResponse) => {
+        return cachedResponse || fetch(e.request);
+      })
+    );
+  });
+  
