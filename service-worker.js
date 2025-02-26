9a5596a2c29e5987c579457a2904395a30a6e1a6
coreventum-frontend <frontend@coreventum.com>
2025-02-26 11:41:44 +0530

    working on pwa

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,43 @@
+const CACHE_NAME = 'calculator-cache-v1';
+
+// Install event
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-      caches.open('calculator-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/CSS/style.css',
-          '/images/Calculator_512.png',
-          '/manifest.json'
-          // Add any other assets that should be cached
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((cachedResponse) => {
-        return cachedResponse || fetch(e.request);
-      })
-    );
-  });
-  
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/index.html',
+        '/CSS/style.css',
+        '/images/Calculator_512.png',
+        '/manifest.json',
+        '/offline.html' // Consider adding an offline page
+      ]);
+    })
+  );
+});
+
+// Activate event - cleanup old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request).catch(() => {
+        // If fetch fails (e.g. offline), return an offline page
+        return caches.match('/offline.html');
+      });
+    })
+  );
+});
